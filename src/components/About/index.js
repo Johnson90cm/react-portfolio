b39cdaec30906a0184eef9838994a01dc4191797
Tank <johnson90.cm@gmail.com>
2022-01-05 00:39:34 -0700

    updated coloring

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,10 +6,10 @@ import "./mysass.scss"
 function About() {
 
     return (
-        <div className="border transparent flex-row scroll">
-            <div className="div-item center">
+        <div className="center border texture flex-row scroll">
+            {/* <div className="div-item center">
                 <img src={selfie} />
-            </div>
+            </div> */}
 
             <div className="div-item center">
                 <p className="center"> Hey there! My name is Colton. I am a recent graduate of the University of Utah's full-stack web development bootcamp. I enjoy creating front end experiences that are both awesome to look at as well as enjoyable to interact with. I am currently looking for web development oppertunities to kickstart my career in the field. If you are looking to get some work done see my contact page. Cheers!</p>
